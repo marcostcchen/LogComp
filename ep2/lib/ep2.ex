a232ac76c76070b7cbc06939e7b6f7cad01955bb
Marcos Chen <marcostcchen@gmail.com>
2021-02-07 17:54:35 -0300

    Ep2 - verificaCadeiaPertence, Inputs, WIP: geraDerivacoes

--- ep2/lib/ep2.ex
+++ ep2/lib/ep2.ex
@@ -3,33 +3,47 @@ defmodule Ep2 do
     defstruct alfa: "a", beta: "abc"
   end
 
-  def verificaSeCadeiaPertenceAGramatica do
+   defmodule Gramatica do
+    defstruct naoTerminais: "AB", terminais: "SBC", leisDeFormacoes: [%LeiDeFormacao{alfa: "S", beta: "aBC"}], simboloInicio: "S"
+  end
+
+  def main do
     # Entrada vai ser a cadeia para teste e a gramatica
     # Gramatica = (naoTerminais, terminais, leisDeFormacoes, simboloInicio)
 
     cadeiaAVerificar = "abc"
 
     naoTerminais = "AB"
-    terminais = "abc"
+    terminais = "SBC"
     leisDeFormacoes = [
-      %LeiDeFormacao{alfa: "A", beta: "abc"},
-      %LeiDeFormacao{alfa: "B", beta: "bcd"}
+      %LeiDeFormacao{alfa: "S", beta: "aBC"},
+      %LeiDeFormacao{alfa: "S", beta: "aSBC"},
+      %LeiDeFormacao{alfa: "CB", beta: "BC"},
+      %LeiDeFormacao{alfa: "aB", beta: "ab"},
+      %LeiDeFormacao{alfa: "bB", beta: "bb"},
+      %LeiDeFormacao{alfa: "bC", beta: "bc"},
+      %LeiDeFormacao{alfa: "cC", beta: "CC"}
     ]
-    simboloInicio = "A"
+    simboloInicio = "S"
+
+    gramatica = %Gramatica{naoTerminais: naoTerminais, terminais: terminais, leisDeFormacoes: leisDeFormacoes, simboloInicio: simboloInicio}
 
     # 1. Funcao que gera as derivacoes
-    listaCadeiasDaGramatica = geraDerivacoes(derivacoes)
+    listaCadeiasDaGramatica = geraDerivacoes(gramatica, String.length(cadeiaAVerificar))
   
     # 2. Comparar se a cadeia de verificacao bate com as cadeias geradas
-    #cadeiaPertence = listaCadeiasDaGramatica.map(cadeia => {
-    #  return cadeia == cadeiaAVerificar
-    #})
-    #return !!!cadeiaPertence
-
+    cadeiaPertence = verificaCadeiaPertence(cadeiaAVerificar, listaCadeiasDaGramatica)
+    IO.inspect(cadeiaPertence)
   end
 
-  def geraDerivacoes (derivacoes) do
+  def geraDerivacoes(gramatica, tamanhoCadeia) do
+    gramatica.naoTerminais
       
   end
 
+  def verificaCadeiaPertence (cadeiaAVerificar, listaCadeiasDaGramatica) do
+    pertence = Enum.find(listaCadeiasDaGramatica, fn cadeiaDaGramatica ->
+      cadeiaDaGramatica == cadeiaAVerificar
+    end)
+  end
 end
